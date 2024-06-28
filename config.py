c005dcf07c293e06fb0fd80a9c3b29739279fac5
Sid1st <97543215+Sid1st@users.noreply.github.com>
2024-06-28 16:29:59 +0530

    Update config.py

--- config.py
+++ config.py
@@ -5,7 +5,7 @@ class Config:
     API_HASH = environ.get("API_HASH", "0d79d297bb8f56caed2c8f08bfc17289")
     BOT_TOKEN = environ.get("BOT_TOKEN", "") 
     BOT_SESSION = environ.get("BOT_SESSION", "MissDaisyy_bot") 
-    DATABASE_URI = "mongodb+srv://MissXone:09ahczILiv7DrtZW@cluster0.nmseyk3.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
+    DATABASE_URI = "mongodb+srv://MissXone:09ahczILiv7DrtZW@cluster0.9msjmml.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
     DATABASE_NAME = environ.get("DATABASE_NAME", "ForwardBot")
     BOT_OWNER_ID = [int(id) for id in environ.get("BOT_OWNER_ID", '5052476013').split()]
 
